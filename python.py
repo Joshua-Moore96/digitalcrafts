4b38f4c440e6976cd8c569b21d120081a9a13be4
Joshua Moore <jvsh98504@gmail.com>
2021-03-10 11:11:39 -0600

    python test push

---
+++ python.py
@@ -0,0 +1,10 @@
+#print("Hello World")
+#print(" Josh, Houston")
+Greeting = ("Hello Digital Crafts Family")
+# print(Greeting)
+classSize = 25
+# print(classSize)
+somethingtoCallIt = ["students", "23", "True"]
+somethingtoCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
+
+print(somethingtoCallIt)
