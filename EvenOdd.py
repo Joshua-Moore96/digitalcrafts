e19f7aba0284be82b94b5d409610cab218592a16
Joshua Moore <jvsh98504@gmail.com>
2021-03-11 07:29:53 -0600

    EvenODD

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,6 +1,8 @@
-number1 = int(input("Enter a number:")
-Answer=number1 % 2
-if (answer=0):
-    priint("Even Number")
+number1 = int(input("Enter a number:"))
+
+answer = number1 % 2
+
+if (answer == 0):
+    print("Even Number")
 else:
     print("Odd Number")
