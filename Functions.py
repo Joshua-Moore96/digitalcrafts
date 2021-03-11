fc815685cb17536814db1e9982919c982779a560
Joshua Moore <jvsh98504@gmail.com>
2021-03-11 10:25:07 -0600

    building functions practice

---
+++ Functions.py
@@ -0,0 +1,22 @@
+def add(num1, num2):
+    result = int(num1) + int(num2)
+    return (result)
+
+
+print(add(2, 2))
+
+
+def sub(num1, num2):
+    result = int(num1) - int(num2)
+    return (result)
+
+
+print(sub(50, 5))
+
+
+def mult(num1, num2):
+    result = int(num1) * int(num2)
+    return (result)
+
+
+print(mult(2, 2))
